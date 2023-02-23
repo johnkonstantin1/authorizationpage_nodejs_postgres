fe5599417c098d4dc517a48d86c4e38ceb5bca10
Anton Bondar <direx187@gmail.com>
2023-02-23 09:50:20 +0200

    final result

--- dbconfig.js
+++ dbconfig.js
@@ -6,19 +6,19 @@ const isProducting = process.env.NOODE_ENV === "production";
 
 // const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-// const pool = new Pool({
-//   connectionString: isProducting ? process.env.DATABASE_URL : connectionString,
-// });
+const pool = new Pool({
+  connectionString: isProducting ? process.env.DATABASE_URL : connectionString,
+});
 // const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
 //   ssl: {
 //     rejectUnauthorized: false,
 //   },
 // });
-const pool = new Pool({
-  connectionString: isProducting ? process.env.DATABASE_URL : connectionString,
-  ssl: isProducting ? { rejectUnauthorized: false } : false,
-});
+// const pool = new Pool({
+//   connectionString: isProducting ? process.env.DATABASE_URL : connectionString,
+//   ssl: isProducting ? { rejectUnauthorized: false } : false,
+// });
 
 module.exports = { pool };
 
